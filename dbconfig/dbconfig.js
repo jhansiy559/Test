109c44f5b68ba75694453a87b9d83268bb54b3d0
jhansi <jhansiy559@gmail.com>
2019-07-09 23:07:53 +0530

    DB Configuration

--- dbconfig/dbconfig.js
+++ dbconfig/dbconfig.js
@@ -1,18 +1,3 @@
-// var mysql = require('mysql');
-
-// var con = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "",
-//   database: "rds_kvb"
-// });
-
-// con.connect(function(err) {
-//     if (err) throw err;
-//     console.log("Connected!");
-//   });
-
-//   module.exports = con;
 
 //Mongodb connection
 
